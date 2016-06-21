9657f69082c1eb1ce9bd99714bc9b60f4ec45ae1
Andrew Manshin <andrew.manshin@me.com>
2016-06-20 20:22:29 -0700

    $10,000 deal tests

--- spec/factory_girls/dealership.rb
+++ spec/factory_girls/dealership.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :dealership do
-    name Faker::Company.name
+    name    Faker::Company.name
+    address Faker::Address.street_address
+    phone   Faker::PhoneNumber.phone_number
   end
 end
