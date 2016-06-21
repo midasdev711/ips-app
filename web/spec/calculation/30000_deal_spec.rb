c014381abee8ab4f96eb14414f7bd5a65ab52a77
Andrew Manshin <andrew.manshin@me.com>
2016-06-21 14:17:47 -0700

    $30,000 deal tests

--- web/spec/calculation/30000_deal_spec.rb
+++ web/spec/calculation/30000_deal_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe '$20,000 deal', type: :calculation do
+RSpec.describe '$30,000 deal', type: :calculation do
 
   let(:dealership) { create :dealership, province_id: 'SK' }
   let(:user) { create :user, dealership: dealership }
@@ -9,7 +9,7 @@ RSpec.describe '$20,000 deal', type: :calculation do
 
   before do
     lender, _ = deal.lenders
-    lender.update bank: Faker::Company.name, loan_type: :finance, cash_price: Money.new(2000000), bank_reg_fee: Money.new(11774), interest_rates: [create(:interest_rate, value: '6.99')]
+    lender.update bank: Faker::Company.name, loan_type: :finance, cash_price: Money.new(3000000), bank_reg_fee: Money.new(11774), interest_rates: [create(:interest_rate, value: '6.99')]
 
     @option = create :option, lender: lender, index: 1, loan_type: :finance, term: 84, payment_frequency: :biweekly, interest_rate: interest_rate
     @option.calculate
@@ -19,17 +19,17 @@ RSpec.describe '$20,000 deal', type: :calculation do
   let(:payment) { @option.categories.last.payment }
 
   [
-    Example.new(6.99, 5881_14, 153_84),
-    Example.new(4.74, 3891_88, 142_91),
-    Example.new(3.99, 3249_42, 139_38),
-    Example.new(3.49, 2827_18, 137_06),
-    Example.new(2.99, 2408_58, 134_76),
-    Example.new(2.49, 1995_44, 132_49),
-    Example.new(1.99, 1585_94, 130_24),
-    Example.new(1.49, 1180_08, 128_01),
-    Example.new(0.99,  779_68, 125_81),
-    Example.new(0.49,  384_74, 123_64),
-    Example.new(0.00,       0, 121_53)
+    Example.new(6.99, 8804_14, 230_34),
+    Example.new(4.74, 5828_44, 213_99),
+    Example.new(3.99, 4865_66, 208_70),
+    Example.new(3.49, 4232_30, 205_22),
+    Example.new(2.99, 3606_22, 201_78),
+    Example.new(2.49, 2987_42, 198_38),
+    Example.new(1.99, 2374_08, 195_01),
+    Example.new(1.49, 1768_02, 191_68),
+    Example.new(0.99, 1167_42, 188_38),
+    Example.new(0.49,  575_92, 185_13),
+    Example.new(0.00,       0, 181_97)
   ].each do |e|
     context "at #{e.rate}%" do
       let(:interest_rate) { e.rate }
