2da5b638d0297e01572a2ab42436600160e709af
Andrew Manshin <andrew.manshin@me.com>
2016-10-26 20:00:40 -0700

    Bye ruby meta class idiom

--- app/services/finance_payment.rb
+++ app/services/finance_payment.rb
@@ -1,13 +1,11 @@
 class FinancePayment
-  class << self
-    def execute(opts)
-      amount, interest_rate, payments_number = opts.values_at :amount, :interest_rate, :payments_number
-      if interest_rate.zero?
-        amount / payments_number
-      else
-        cents = (amount.cents * (interest_rate + interest_rate / ((1 + interest_rate) ** payments_number - 1))).ceil
-        Money.new(cents)
-      end
+  def self.execute(opts)
+    amount, interest_rate, payments_number = opts.values_at :amount, :interest_rate, :payments_number
+    if interest_rate.zero?
+      amount / payments_number
+    else
+      cents = (amount.cents * (interest_rate + interest_rate / ((1 + interest_rate) ** payments_number - 1))).ceil
+      Money.new(cents)
     end
   end
 end
