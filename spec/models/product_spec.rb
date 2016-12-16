131925d06d84a6e43695ba1bda69575744e98781
Andrew Manshin <andrew.manshin@me.com>
2016-12-15 20:02:32 -0800

    Product#tax_rate unit tests.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -45,37 +45,83 @@ RSpec.describe Product, type: :model do
     end
   end
 
-  describe '#amount' do
-    subject { product.send :amount }
-
-    let(:product)  { build :product }
-    let(:money)    { double :money }
-    let(:tax_rate) { double :tax_rate }
-
-    before do
-      allow(product).to receive(:retail_price).and_return money
-      allow(product).to receive(:tax_rate).and_return tax_rate
-
-      allow(money).to receive(:+).with(money).and_return money
-      allow(money).to receive(:*).with(tax_rate).and_return money
-    end
-
-    it { is_expected.to eq money }
-  end
-
-  describe '#profit' do
-    subject { product.profit }
-
+  describe 'instance methods' do
     let(:product) { build :product }
-    let(:money)   { double :money }
 
-    before do
-      allow(product).to receive(:retail_price).and_return money
-      allow(product).to receive(:dealer_cost).and_return money
+    describe '#amount' do
+      subject { product.send :amount }
 
-      allow(money).to receive(:-).with(money).and_return money
+      let(:money)    { double :money }
+      let(:tax_rate) { double :tax_rate }
+
+      before do
+        allow(product).to receive(:retail_price).and_return money
+        allow(product).to receive(:tax_rate).and_return tax_rate
+
+        allow(money).to receive(:+).with(money).and_return money
+        allow(money).to receive(:*).with(tax_rate).and_return money
+      end
+
+      it { is_expected.to eq money }
     end
 
-    it { is_expected.to eq money }
+    describe '#profit' do
+      subject { product.profit }
+
+      let(:money)   { double :money }
+
+      before do
+        allow(product).to receive(:retail_price).and_return money
+        allow(product).to receive(:dealer_cost).and_return money
+
+        allow(money).to receive(:-).with(money).and_return money
+      end
+
+      it { is_expected.to eq money }
+    end
+
+    describe '#tax_rate' do
+      subject { product.send :tax_rate }
+
+      let(:product_list)  { double :product_list, listable: deal }
+      let(:deal)          { double :deal, province: province, status_indian: status_indian }
+      let(:province)      { double :province, gst: gst, pst: pst }
+      let(:gst)           { 0.05 }
+      let(:pst)           { 0.07 }
+
+      let(:status_indian) { false }
+
+      let(:no_tax)        { 0.0 }
+      let(:one_tax)       { gst }
+      let(:two_tax)       { gst + pst }
+
+      before do
+        allow(product).to receive(:product_list).and_return product_list
+      end
+
+      context 'status indian' do
+        let(:status_indian) { true }
+
+        it { is_expected.to eq no_tax }
+      end
+
+      context 'no tax' do
+        let(:product) { build :product, tax: 'no' }
+
+        it { is_expected.to eq no_tax }
+      end
+
+      context 'one tax' do
+        let(:product) { build :product, tax: 'one' }
+
+        it { is_expected.to eq one_tax }
+      end
+
+      context 'two tax' do
+        let(:product) { build :product, tax: 'two' }
+
+        it { is_expected.to eq two_tax }
+      end
+    end
   end
 end
