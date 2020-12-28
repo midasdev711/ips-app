711f0d36bedefec38dba973d16b920857c8c23ff
Midas Dev <midasdev711@gmail.com>
2020-12-28 05:10:05 -0500

    update logo, images, layout and video page

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -2,13 +2,14 @@ class VideoController < ApplicationController
   
   def index
     vimeo_client = VimeoMe2::VimeoObject.new('4123f8e58da9644c05d74773b1fea627')
-    res = vimeo_client.get('/users/129575771/projects/3259111/videos?per_page=50&page=1')
+    res = vimeo_client.get('/users/129575771/projects/3259111/videos?direction=asc&per_page=50&page=1&sort=date')
     videolist = []
-    res["data"].each { |video| 
+    res["data"].each.with_index(1) { |video, index| 
       uri = video["uri"].split("/")
       videolist.push({
         "id" => uri[2].split(":")[0],
-        "name" => video["name"],
+        "index" => "#{index}. ",
+        "name" => "#{video['name']}",
         "thumbnail" => video["pictures"]["sizes"][2]["link"]
       })
     }
