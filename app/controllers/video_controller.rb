ad89ce7ac43e107e42cfd7e209fa0f8b2bcd7664
Midas Dev <midasdev711@gmail.com>
2021-04-05 04:36:35 -0400

    fix issue in video sorting

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -2,7 +2,7 @@ class VideoController < ApplicationController
   
   def index
     vimeo_client = VimeoMe2::VimeoObject.new('4123f8e58da9644c05d74773b1fea627')
-    res = vimeo_client.get('/users/129575771/projects/3259111/videos?direction=asc&per_page=50&page=1&sort=name')
+    res = vimeo_client.get('/users/129575771/projects/3259111/videos?direction=asc&per_page=50&page=1&sort=alphabetical')
     videolist = []
     res["data"].each.with_index(1) { |video, index| 
       uri = video["uri"].split("/")
